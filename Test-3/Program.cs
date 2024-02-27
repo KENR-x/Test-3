6982d4c877cb2f0c669e7fccd8af6f0683577768
Человек <@DESKTOP-8PQMG9H>
2024-02-27 21:38:03 +0300

    Добавьте файлы проекта.

---
+++ Test-3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Test
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите значение x: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+        double y = (double)(x - 10) / (x * 10);
+        Console.WriteLine("Число:  " + y);
+
+    }
+}
